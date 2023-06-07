07f5e71fe91022e46bf94eab2226a6548619455c
jcisl <jcisl@HP-OMEN-30L>
2023-06-07 21:05:25 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp {
+  class Program {
+    static void Main(string[] args) {
+      Console.Write("Enter your name: ");
+      string name = Console.ReadLine();
+      Console.WriteLine("Hello, " + name + "! " + name.Contains("Joshua"));
+      Console.ReadLine();
+      // gpt Write hello 10 times
+      // You exceeded your current quota, please check your plan and billing details.
+
+
+
+    }
+  }
+}
